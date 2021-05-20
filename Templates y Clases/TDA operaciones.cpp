40885fd4856a596ade3de50b74da2fc23c2be13b
andrespallango <andy.pal001@gmail.com>
2021-05-20 18:57:36 -0500

    Creation of "operaciones.h"

--- Templates y Clases/TDA operaciones.cpp
+++ Templates y Clases/TDA operaciones.cpp
@@ -10,58 +10,9 @@
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
+#include "operaciones.h"
+
 using namespace std;
-template <typename tipoDato>
-class Operaciones {
-private:
-	tipoDato valor1;
-	tipoDato valor2;
-public:
-	Operaciones(tipoDato _valor1, tipoDato _valor2) {
-		valor1 = _valor1;
-		valor2 = _valor2;
-	}
-	// Setters
-	tipoDato setValor1(int _valor1) {
-		valor1 = _valor1;
-	}
-	tipoDato setValor2(int _valor2) {
-		valor2 = _valor2;
-	}
-
-	// Getters
-	tipoDato getValor1() {
-		return valor1;
-	}
-	tipoDato getValor2() {
-		return valor2;
-	}
-
-	tipoDato suma();
-	tipoDato resta();
-	tipoDato multiplicacion();
-	tipoDato division();
-};
-template <typename tipoDato>
-tipoDato Operaciones<tipoDato>::suma() {
-	return valor1 + valor2;
-}
-template <typename tipoDato>
-tipoDato Operaciones<tipoDato>::resta() {
-	return valor1 - valor2;
-}
-template <typename tipoDato>
-tipoDato Operaciones<tipoDato>::multiplicacion() {
-	return valor1 * valor2;
-}
-template <typename tipoDato>
-tipoDato Operaciones<tipoDato>::division() {
-	if (valor2 == 0) {
-		cout << "No se puede dividir" << endl;
-	}
-	else
-		return valor1 / valor2;
-}
 
 int main(int argc, char** argv) {
 	float valor1, valor2;
---
+++ Templates y Clases/operaciones.h
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+template <typename tipoDato>
+class Operaciones {
+private:
+	tipoDato valor1;
+	tipoDato valor2;
+public:
+	Operaciones(tipoDato _valor1, tipoDato _valor2) {
+		valor1 = _valor1;
+		valor2 = _valor2;
+	}
+	// Setters
+	tipoDato setValor1(int _valor1) {
+		valor1 = _valor1;
+	}
+	tipoDato setValor2(int _valor2) {
+		valor2 = _valor2;
+	}
+
+	// Getters
+	tipoDato getValor1() {
+		return valor1;
+	}
+	tipoDato getValor2() {
+		return valor2;
+	}
+
+	tipoDato suma();
+	tipoDato resta();
+	tipoDato multiplicacion();
+	tipoDato division();
+};
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::suma() {
+	return getValor1() + getValor2();
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::resta() {
+	return getValor1() - getValor2();
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::multiplicacion() {
+	return getValor1() * getValor2();
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::division() {
+	if (getValor2() == 0) {
+		cout << "No se puede dividir" << endl;
+	}
+	else
+		return getValor1() / getValor2();
+}
