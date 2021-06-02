0e4323b59fef3e88bc4d326a0ae4df09fb480a57
RenatoNavas <84428509+RenatoNavas@users.noreply.github.com>
2021-06-01 23:07:48 -0500

    Add files via upload

---
+++ Vector con Datos Abstractos/Vector.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+class Vector {
+	private:
+		int vect[10];
+	public:
+		Vector(int [10]);
+		Vector()=default;
+		void set_vect(int[10]);
+		int *get_vect();	
+};
