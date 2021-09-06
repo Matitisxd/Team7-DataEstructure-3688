33c0f9901894b9ad2f52fd18fcd23b9adaf0b6a9
andrespallango <51092948+andrespallango@users.noreply.github.com>
2021-09-06 10:04:58 -0500

    Laberinto_backingtrack

---
+++ Parcial 3/Laberinto/ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+ 
+{
+    //Creamos dos variables un "int" y "char"
+    char a=178;
+ 
+    //Mostramos "char" 
+    cout <<"El caracter es: "<<a<<endl;
+ 
+    return 0;
+}
